c38fbb564fc4495d5d948f1602464fa1dde67481
Wayden03 <iscordero@estudiantec.cr>
2024-04-14 17:54:10 -0600

    actu 2
    Listos los graficos unidimensionales

--- proyecto.r
+++ proyecto.r
@@ -1,4 +1,7 @@
 library(ggplot2)
+library(dplyr)
+library(ggmosaic)
+library(patchwork)
 
 data <- read.csv(file.choose(),header=TRUE,sep = ";")
 attach(data)
@@ -8,3 +11,12 @@ print(names(data))
 plot<- ggplot(data, aes(Clase.de.accidente, fill=Clase.de.accidente))+geom_bar()+
     ggtitle("Clase de Accidente")
 print(plot)
+
+plot2<-ggplot(data, aes(Tipo.de.accidente, fill=Tipo.de.accidente))+geom_bar()+
+    ggtitle("Tipo de accidente")
+print(plot2)
+
+
+plot3<-ggplot(data, aes(Provincia, fill=Provincia))+geom_bar()+
+    ggtitle("Año")
+print(plot3)
