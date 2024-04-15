ffa88dc0b513eab811570a0337379a731eacbf58
Wayden03 <iscordero@estudiantec.cr>
2024-04-14 18:03:13 -0600

    Actu 3
    Cambio por pie chart

--- proyecto.r
+++ proyecto.r
@@ -10,13 +10,15 @@ print(names(data))
 
 plot<- ggplot(data, aes(Clase.de.accidente, fill=Clase.de.accidente))+geom_bar()+
     ggtitle("Clase de Accidente")
-print(plot)
+#print(plot)
 
-plot2<-ggplot(data, aes(Tipo.de.accidente, fill=Tipo.de.accidente))+geom_bar()+
-    ggtitle("Tipo de accidente")
+plot2<-ggplot(data, aes(x="", y=Tipo.de.accidente, fill=Tipo.de.accidente)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)+
+  ggtitle("Tipo de accidente")
 print(plot2)
 
 
 plot3<-ggplot(data, aes(Provincia, fill=Provincia))+geom_bar()+
-    ggtitle("Año")
-print(plot3)
+    ggtitle("Provincia")
+#print(plot3)
